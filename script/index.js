49d2b8926d3925ee427137055e096b4f3a88ec6e
Junior Programmer <progphyton@gmail.com>
2022-05-10 01:17:46 +0300

    feat: add script file

---
+++ script/index.js
@@ -0,0 +1,529 @@
+const keydata = {
+  key_eng: {
+    Backquote: '`',
+    Digit1: '1',
+    Digit2: '2',
+    Digit3: '3',
+    Digit4: '4',
+    Digit5: '5',
+    Digit6: '6',
+    Digit7: '7',
+    Digit8: '8',
+    Digit9: '9',
+    Digit0: '0',
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'q',
+    KeyW: 'w',
+    KeyE: 'e',
+    KeyR: 'r',
+    KeyT: 't',
+    KeyY: 'y',
+    KeyU: 'u',
+    KeyI: 'i',
+    KeyO: 'o',
+    KeyP: 'p',
+    BracketLeft: '[',
+    BracketRight: ']',
+    Backslash: '\\',
+    Delete: 'Del',
+    CapsLock: 'Caps Lock',
+    KeyA: 'a',
+    KeyS: 's',
+    KeyD: 'd',
+    KeyF: 'f',
+    KeyG: 'g',
+    KeyH: 'h',
+    KeyJ: 'j',
+    KeyK: 'k',
+    KeyL: 'l',
+    Semicolon: ';',
+    Quote: '\'',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'z',
+    KeyX: 'x',
+    KeyC: 'c',
+    KeyV: 'v',
+    KeyB: 'b',
+    KeyN: 'n',
+    KeyM: 'm',
+    Comma: ',',
+    Period: '.',
+    Slash: '/',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    MetaLeft: 'Win',
+    AltLeft: 'Alt',
+    Space: 'Space',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
+  eng_caps: {
+    Backquote: '~',
+    Digit1: '!',
+    Digit2: '@',
+    Digit3: '#',
+    Digit4: '$',
+    Digit5: '%',
+    Digit6: '^',
+    Digit7: '&',
+    Digit8: '*',
+    Digit9: '(',
+    Digit0: ')',
+    Minus: '_',
+    Equal: '+',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'Q',
+    KeyW: 'W',
+    KeyE: 'E',
+    KeyR: 'R',
+    KeyT: 'T',
+    KeyY: 'Y',
+    KeyU: 'U',
+    KeyI: 'I',
+    KeyO: 'O',
+    KeyP: 'P',
+    BracketLeft: '{',
+    BracketRight: '}',
+    Backslash: '|',
+    Delete: 'Del',
+    CapsLock: 'Caps Lock',
+    KeyA: 'A',
+    KeyS: 'S',
+    KeyD: 'D',
+    KeyF: 'F',
+    KeyG: 'G',
+    KeyH: 'H',
+    KeyJ: 'J',
+    KeyK: 'K',
+    KeyL: 'L',
+    Semicolon: ':',
+    Quote: '"',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'Z',
+    KeyX: 'X',
+    KeyC: 'C',
+    KeyV: 'V',
+    KeyB: 'B',
+    KeyN: 'N',
+    KeyM: 'M',
+    Comma: '<',
+    Period: '>',
+    Slash: '?',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    MetaLeft: 'Win',
+    AltLeft: 'Alt',
+    Space: 'Space',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
+  key_rus: {
+    Backquote: 'ё',
+    Digit1: '1',
+    Digit2: '2',
+    Digit3: '3',
+    Digit4: '4',
+    Digit5: '5',
+    Digit6: '6',
+    Digit7: '7',
+    Digit8: '8',
+    Digit9: '9',
+    Digit0: '0',
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'й',
+    KeyW: 'ц',
+    KeyE: 'у',
+    KeyR: 'к',
+    KeyT: 'е',
+    KeyY: 'н',
+    KeyU: 'г',
+    KeyI: 'ш',
+    KeyO: 'щ',
+    KeyP: 'з',
+    BracketLeft: 'х',
+    BracketRight: 'ъ',
+    Backslash: '\\',
+    Delete: 'Del',
+    CapsLock: 'Caps Lock',
+    KeyA: 'ф',
+    KeyS: 'ы',
+    KeyD: 'в',
+    KeyF: 'а',
+    KeyG: 'п',
+    KeyH: 'р',
+    KeyJ: 'о',
+    KeyK: 'л',
+    KeyL: 'д',
+    Semicolon: 'ж',
+    Quote: 'э',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'я',
+    KeyX: 'ч',
+    KeyC: 'с',
+    KeyV: 'м',
+    KeyB: 'и',
+    KeyN: 'т',
+    KeyM: 'ь',
+    Comma: 'б',
+    Period: 'ю',
+    Slash: '.',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    MetaLeft: 'Win',
+    AltLeft: 'Alt',
+    Space: 'Space',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
+  rus_caps: {
+    Backquote: 'Ё',
+    Digit1: '!',
+    Digit2: '"',
+    Digit3: '№',
+    Digit4: ';',
+    Digit5: '%',
+    Digit6: ':',
+    Digit7: '?',
+    Digit8: '*',
+    Digit9: '(',
+    Digit0: ')',
+    Minus: '_',
+    Equal: '+',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'Й',
+    KeyW: 'Ц',
+    KeyE: 'У',
+    KeyR: 'К',
+    KeyT: 'Е',
+    KeyY: 'Н',
+    KeyU: 'Г',
+    KeyI: 'Ш',
+    KeyO: 'Щ',
+    KeyP: 'З',
+    BracketLeft: 'Х',
+    BracketRight: 'Ъ',
+    Backslash: '/',
+    Delete: 'Del',
+    CapsLock: 'Caps Lock',
+    KeyA: 'Ф',
+    KeyS: 'Ы',
+    KeyD: 'В',
+    KeyF: 'А',
+    KeyG: 'П',
+    KeyH: 'Р',
+    KeyJ: 'О',
+    KeyK: 'Л',
+    KeyL: 'Д',
+    Semicolon: 'Ж',
+    Quote: 'Э',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'Я',
+    KeyX: 'Ч',
+    KeyC: 'С',
+    KeyV: 'М',
+    KeyB: 'И',
+    KeyN: 'Т',
+    KeyM: 'Ь',
+    Comma: 'Б',
+    Period: 'Ю',
+    Slash: ',',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    MetaLeft: 'Win',
+    AltLeft: 'Alt',
+    Space: 'Space',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
+};
+
+const keycode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+
+class KeyBoard {
+  constructor(lang = 'key_eng') {
+    this.key_caps = false;
+    this.key_lang = lang;
+    if (localStorage.getItem('jun#2774VkbLang')) {
+      this.key_lang = localStorage.getItem('jun#2774VkbLang');
+    } else {
+      this.key_lang = 'key_eng';
+    }
+  }
+
+  createKeyBoardFrame() {
+    const elementBody = document.body;
+    elementBody.classList.add('body');
+    elementBody.insertAdjacentHTML('afterbegin', '<div class="wrap"></div>');
+    const elementWrap = document.querySelector('.wrap');
+    elementWrap.insertAdjacentHTML('beforeend', '<textarea class="textarea" id="AREA" readonly></textarea>');
+    elementWrap.insertAdjacentHTML('beforeend', '<p class="inform">Switch language: Left Alt + Left Shift. Designed for Windows</p>');
+    elementWrap.insertAdjacentHTML('beforeend', '<div class="keyboard" id="VKB"></div>');
+    this.elementVKB = document.querySelector('.keyboard');
+    this.createKeysFragments();
+  }
+
+  createKeysFragments() {
+    const keyFragment = document.createDocumentFragment();
+    for (let i = 0; i <= 63; i += 1) {
+      const item = document.createElement('div');
+      item.setAttribute('class', 'vkbkeys');
+      if (i === 13 || i === 29 || i === 41 || i === 42 || i === 54) {
+        item.classList.add('keysize_3');
+      } else if (i === 0 || i === 14 || i === 28 || i === 53) {
+        item.classList.add('keysize_1x');
+      } else if (i === 55 || i === 59) {
+        item.classList.add('keysize_2');
+      } else if (i === 58) {
+        item.classList.add('keysize_4');
+      } else {
+        item.classList.add('keysize_1');
+      }
+      if (i > 54 && i < 58) {
+        item.classList.add('keyscolor');
+      } else if (i > 58 && i < 64) {
+        item.classList.add('keyscolor');
+      }
+      item.setAttribute('id', `${keycode[i]}`);
+      keyFragment.append(item);
+    }
+    this.elementVKB.append(keyFragment);
+    this.createKeysLayout();
+  }
+
+  createKeysLayout() {
+    let j = 0;
+    const nodeVKB = document.getElementById('VKB');
+    if (this.key_lang === 'key_eng' && this.key_caps === false) {
+      for (const child of nodeVKB.children) {
+        child.textContent = keydata.key_eng[`${keycode[j]}`];
+        j += 1;
+      }
+    } else if (this.key_lang === 'key_rus' && this.key_caps === false) {
+      for (const child of nodeVKB.children) {
+        child.textContent = keydata.key_rus[`${keycode[j]}`];
+        j += 1;
+      }
+    } else if (this.key_lang === 'key_eng' && this.key_caps === true) {
+      for (const child of nodeVKB.children) {
+        child.textContent = keydata.eng_caps[`${keycode[j]}`];
+        j += 1;
+      }
+    } else if (this.key_lang === 'key_rus' && this.key_caps === true) {
+      for (const child of nodeVKB.children) {
+        child.textContent = keydata.rus_caps[`${keycode[j]}`];
+        j += 1;
+      }
+    }
+  }
+
+  printCurrentKey(code) {
+    if (this.key_lang === 'key_eng' && this.key_caps === false) {
+      return keydata.key_eng[`${code}`];
+    } if (this.key_lang === 'key_rus' && this.key_caps === false) {
+      return keydata.key_rus[`${code}`];
+    } if (this.key_lang === 'key_eng' && this.key_caps === true) {
+      return keydata.eng_caps[`${code}`];
+    } if (this.key_lang === 'key_rus' && this.key_caps === true) {
+      return keydata.rus_caps[`${code}`];
+    }
+  }
+
+  addEventsListeners() {
+    document.addEventListener('keydown', (event) => this.eventKeyDown(event));
+    document.addEventListener('keyup', (event) => this.eventKeyUp(event));
+    document.addEventListener('mousedown', (event) => this.eventMouseDown(event));
+    document.addEventListener('mouseup', (event) => this.eventMouseUp(event));
+  }
+
+  eventKeyDown(event) {
+    const nodeAREA = document.getElementById('AREA');
+    // initial statement
+    if (keycode.includes(event.code)) {
+      event.preventDefault();
+    } else {
+      return;
+    }
+    // keys highlight
+    if (keycode.includes(event.code)) {
+      document.getElementById(`${event.code}`).classList.add('keyactive');
+    } else {
+      return;
+    }
+    // switch language
+    if (((event.code === 'ShiftLeft') && event.altKey) || (event.shiftKey && (event.code === 'AltLeft'))) {
+      this.key_lang === 'key_eng' ? this.key_lang = 'key_rus' : this.key_lang = 'key_eng';
+      localStorage.setItem('jun#2774VkbLang', this.key_lang);
+      this.createKeysLayout();
+    }
+    // events caps down
+    if (event.code === 'CapsLock') {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+    // events shift down
+    if (event.key === 'Shift') {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+    // special keys
+    switch (event.code) {
+      // can be used textContent
+      case 'Backspace':
+        nodeAREA.value = nodeAREA.value.slice(0, -1);
+        break;
+      case 'Tab':
+        nodeAREA.value += '	';
+        break;
+      case 'Delete':
+        nodeAREA.selectionStart = 0;
+        nodeAREA.value = nodeAREA.value.slice(1, -1);
+        break;
+      case 'Enter':
+        nodeAREA.value += '\n';
+        break;
+      case 'Space':
+        nodeAREA.value += ' ';
+        break;
+      case 'CapsLock':
+      case 'ShiftLeft':
+      case 'ShiftRight':
+      case 'ControlLeft':
+      case 'MetaLeft':
+      case 'AltLeft':
+      case 'AltRight':
+      case 'ControlRight':
+        break;
+      default:
+        if (keycode.includes(event.code)) {
+          nodeAREA.value += this.printCurrentKey(event.code);
+        } else {
+          break;
+        }
+        break;
+    }
+  }
+
+  eventMouseDown(event) {
+    const nodeAREA = document.getElementById('AREA');
+    // initial statement
+    if (keycode.includes(event.target.id)) {
+      event.preventDefault();
+    } else {
+      return;
+    }
+    // mouse caps down
+    if (event.target.id === 'CapsLock') {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+    // events shift down
+    if ((event.target.id === 'ShiftLeft') || (event.target.id === 'ShiftRight')) {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+    // special keys
+    switch (event.target.id) {
+      // can be used textContent
+      case 'Backspace':
+        nodeAREA.value = nodeAREA.value.slice(0, -1);
+        break;
+      case 'Tab':
+        nodeAREA.value += '	';
+        break;
+      case 'Delete':
+        nodeAREA.selectionStart = 0;
+        nodeAREA.value = nodeAREA.value.slice(1, -1);
+        break;
+      case 'Enter':
+        nodeAREA.value += '\n';
+        break;
+      case 'Space':
+        nodeAREA.value += ' ';
+        break;
+      case 'CapsLock':
+      case 'ShiftLeft':
+      case 'ShiftRight':
+      case 'ControlLeft':
+      case 'MetaLeft':
+      case 'AltLeft':
+      case 'AltRight':
+      case 'ControlRight':
+        break;
+      default:
+        if (keycode.includes(event.target.id)) {
+          nodeAREA.value += this.printCurrentKey(event.target.id);
+        } else {
+          break;
+        }
+        break;
+    }
+  }
+
+  eventKeyUp(event) {
+    // initial statement
+    if (keycode.includes(event.code)) {
+      event.preventDefault();
+    } else {
+      return;
+    }
+    // keys highlight
+    if (keycode.includes(event.code)) {
+      document.getElementById(`${event.code}`).classList.remove('keyactive');
+    } else {
+      return;
+    }
+    // events shift up
+    if (event.key === 'Shift') {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+  }
+
+  eventMouseUp(event) {
+    // initial statement
+    if (keycode.includes(event.target.id)) {
+      event.preventDefault();
+    } else {
+      return;
+    }
+    // mouse shift up
+    if ((event.target.id === 'ShiftLeft') || (event.target.id === 'ShiftRight')) {
+      this.key_caps === false ? this.key_caps = true : this.key_caps = false;
+      this.createKeysLayout();
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const VKB = new KeyBoard('key_eng');
+  VKB.createKeyBoardFrame();
+  VKB.addEventsListeners();
+});
